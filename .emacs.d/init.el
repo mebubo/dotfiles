12942bb7fedf29b62f8c15f89cd7e66ba26da496
Sergei Dolgov <dolgovs@gmail.com>
2012-03-27 14:47:30 +0200

    Add emacs init.el file for my new starter-kit-based configuration

---
+++ .emacs.d/init.el
@@ -0,0 +1,19 @@
+(set-frame-font "monospace-11")
+
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(setq org-publish-project-alist
+      '(("mebubo.github.com"
+         :base-directory "~/j/mebubo.github.com/org/"
+         :base-extension "org"
+         :publishing-directory "~/j/mebubo.github.com/_posts/"
+         :recursive t
+         :publishing-function org-publish-org-to-html
+         :headline-levels 4
+         :html-extension "html"
+         :body-only t)))
+
+(server-start)
