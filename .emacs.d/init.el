bc20c68339e50c0a9eccb6bb566dd4808f3a13a9
Sergei Dolgov <dolgovs@gmail.com>
2014-10-28 13:43:15 +0100

    emacs: adjust repo urls

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,9 +22,9 @@
 ;; packages
 (with-library 'package
               (add-to-list 'package-archives
-                           '("marmalade" . "http://marmalade-repo.org/packages/") t)
+                           '("marmalade" . "https://marmalade-repo.org/packages/") t)
               (add-to-list 'package-archives
-                           '("melpa" . "http://melpa.milkbox.net/packages/") t)
+                           '("melpa" . "http://melpa.org/packages/") t)
               (package-initialize)
 
               (when (not package-archive-contents)
