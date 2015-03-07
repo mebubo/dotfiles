4dbbdce6e2eb8287de98e04158a16c6b83ef4e01
Sergei Dolgov <dolgovs@gmail.com>
2015-03-07 21:18:48 +0100

    emacs: don't use marmalade repo

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,8 +22,6 @@
 ;; packages
 (with-library 'package
               (add-to-list 'package-archives
-                           '("marmalade" . "https://marmalade-repo.org/packages/") t)
-              (add-to-list 'package-archives
                            '("melpa" . "http://melpa.org/packages/") t)
               (package-initialize)
 
