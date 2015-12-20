3a6fe4e70bc39d0a80cb214552277522d2939dc2
Sergei Dolgov <dolgovs@gmail.com>
2015-12-20 15:59:19 +0100

    emacs: copy more stuff from the old config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,6 +2,8 @@
   `(when (require ,symbol nil t)
      ,@body))
 
+(set-register ?i '(file . "~/.emacs.d/init.el"))
+
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file 'noerror)
 
@@ -45,22 +47,30 @@
 
 (setq inhibit-startup-message t
       visible-bell 1
-      kill-whole-line t)
+      kill-whole-line t
+      frame-title-format "emacs: %b %f")
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(global-set-key [C-tab] 'other-window)
 
 (blink-cursor-mode 0)
 
+(column-number-mode 1)
+
 (global-set-key (kbd "C-z") nil)
-
-(ido-mode 'buffers)
-(global-set-key (kbd "M-l") 'ido-switch-buffer)
-(add-to-list 'ido-ignore-buffers "^\*")
-
-(with-library 'helm
-              (require 'helm-config))
 
 (savehist-mode 1)
 
 (server-start)
+
+(desktop-save-mode 1)
+
+;;
+
+(ido-mode 'buffers)
+(global-set-key (kbd "M-l") 'ido-switch-buffer)
+(add-to-list 'ido-ignore-buffers "^\*")
 
 (setq winner-dont-bind-my-keys t)
 (winner-mode 1)
@@ -75,6 +85,8 @@
       browse-url-generic-program "google-chrome")
 
 (with-library 'whole-line-or-region
+              (add-to-list 'whole-line-or-region-extensions-alist
+                           '(comment-dwim whole-line-or-region-comment-dwim nil))
               (whole-line-or-region-mode))
 
 (with-library 'helm
@@ -87,11 +99,12 @@
               (setq helm-split-window-in-side-p t)
               )
 
-(global-set-key (kbd "C-c l") 'org-store-link)
-(global-set-key (kbd "C-c c") 'org-capture)
-(global-set-key (kbd "C-c a") 'org-agenda)
-(setq org-directory "~/dev/txt/"
-      org-default-notes-file (concat org-directory "refile.org"))
+(with-library 'org
+              (global-set-key (kbd "C-c l") 'org-store-link)
+              (global-set-key (kbd "C-c c") 'org-capture)
+              (global-set-key (kbd "C-c a") 'org-agenda)
+              (setq org-directory "~/dev/txt/"
+                    org-default-notes-file (concat org-directory "refile.org")))
 
 
 (with-library 'haskell-mode
@@ -106,7 +119,27 @@
               (with-library 'ghc
                             (autoload 'ghc-init "ghc" nil t)
                             (autoload 'ghc-debug "ghc" nil t)
-                            (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
-                            )
-              )
+                            (add-hook 'haskell-mode-hook (lambda () (ghc-init)))))
 
+(with-library 'highlight-symbol
+              (global-set-key (kbd "C-<f3>") 'highlight-symbol-at-point)
+              (global-set-key (kbd "C-<f4>") 'highlight-symbol-remove-all)
+              (global-set-key (kbd "C-x *") 'highlight-symbol-prev)
+              (global-set-key (kbd "C-*") 'highlight-symbol-next)
+              (setq highlight-symbol-on-navigation-p t))
+
+(with-library 'expand-region
+              (global-set-key (kbd "C-=") 'er/expand-region))
+
+(with-library 'back-button
+              (back-button-mode 1))
+
+(with-library 'evil
+	      (evil-mode 1)
+	      (setq evil-default-state 'emacs)
+              (global-undo-tree-mode -1))
+
+(with-library 'guide-key
+              (setq guide-key/guide-key-sequence '("C-c" "C-x"))
+              (setq guide-key/recursive-key-sequence-flag t)
+              (guide-key-mode 1))
