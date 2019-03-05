290898e8c8a028e20050d2350241cc7e4e269af9
Sergei Dolgov <dolgovs@gmail.com>
2019-03-05 08:18:54 +0100

    nix: haskell: use 8.6.3

--- .config/nixpkgs/overlays/50-haskell.nix
+++ .config/nixpkgs/overlays/50-haskell.nix
@@ -1,14 +1,9 @@
 self: super:
 with super.haskell.lib;
 {
-  haskellPackages = super.haskellPackages // {
-    codex = super.haskellPackages.callPackage /home/me/src/codex {};
-    grid = doJailbreak super.haskellPackages.grid;
-    heist = dontCheck super.haskellPackages.heist;
-    hasktags = dontCheck super.haskellPackages.hasktags;
-  };
+  h = super.haskell.packages.ghc863;
 
-  myHaskellEnv = super.haskell.packages.ghc862.ghcWithPackages
+  myHaskellEnv = super.haskell.packages.ghc863.ghcWithPackages
   (haskellPackages: with haskellPackages; [
     mtl transformers lens recursion-schemes
   ]);
