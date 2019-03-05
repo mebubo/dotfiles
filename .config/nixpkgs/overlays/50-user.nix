03b582bcb5eca7cebb2474a7d42df942cd5d584e
Sergei Dolgov <dolgovs@gmail.com>
2019-03-05 08:05:53 +0100

    nix: nx-rebuild: update packages

--- .config/nixpkgs/overlays/50-user.nix
+++ .config/nixpkgs/overlays/50-user.nix
@@ -10,45 +10,60 @@ self: super:
   } // (with self; {
     inherit
     ag
+    bat
+    coursier
     ctags
     curl
     dmenu
+    exa
+    fd
     feh
+    ffmpeg
     file
     firefox-bin
+    fish
     git
     gnupg
     google-chrome
+    grim
     htop
     i3status
     jq
+    miniserve
     moreutils
     mpv
+    nodejs
     pavucontrol
+    psc-package
     pstree
     pwgen
     ripgrep
-    sbt-extras
+    sbt
     sqlite
+    slurp
     st
     sway-beta
+    swayidle
+    swaylock
     tmux
     tree
     unzip
     usbutils
     vim
+    vscode
     wget
     wlroots
     youtube-dl
     zip
     ;
-    idea-community = jetbrains.idea-community_2018_2;
+    # idea-community = jetbrains.idea-community;
   }) // (with self.haskell.packages.ghc863; {
     inherit
     cabal-install
     cabal2nix
     ghcid
     hasktags
+    hoogle
     hpack
     nix-derivation
     stack
