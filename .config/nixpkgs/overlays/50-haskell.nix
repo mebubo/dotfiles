11b267658afdad59eb2b36e8b501e2535196ca1d
Sergei Dolgov <dolgovs@gmail.com>
2019-04-30 20:30:04 +0200

    nix: use ghc864

--- .config/nixpkgs/overlays/50-haskell.nix
+++ .config/nixpkgs/overlays/50-haskell.nix
@@ -1,9 +1,9 @@
 self: super:
 with super.haskell.lib;
 {
-  h = super.haskell.packages.ghc863;
+  h = super.haskell.packages.ghc864;
 
-  myHaskellEnv = super.haskell.packages.ghc863.ghcWithPackages
+  myHaskellEnv = self.h.ghcWithPackages
   (haskellPackages: with haskellPackages; [
     mtl transformers lens recursion-schemes
   ]);
