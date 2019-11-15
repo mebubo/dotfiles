9134beb8b283ea355e0871316bc5c06edb68086e
Sergei Dolgov <dolgovs@gmail.com>
2019-11-15 23:16:37 +0100

    nixpkgs: snapshot

--- .config/nixpkgs/overlays/50-user.nix
+++ .config/nixpkgs/overlays/50-user.nix
@@ -7,28 +7,36 @@ self: super:
       exec ${self.nix}/bin/nix-env -f '<nixpkgs>' -j1 -r -iA user-packages "$@"
     '';
 
+    xauth = self.xorg.xauth;
+    xorgserver = self.xorg.xorgserver;
+
   } // (with self; {
     inherit
     ag
     alacritty
     bat
+    cage
+    # chromium
     coursier
     ctags
     curl
     dmenu
+    # evince
     exa
     fd
     feh
     ffmpeg_4
     file
     firefox-wayland
-    fish
+    # fish
     git
     gnupg
     google-chrome
     grim
     htop
+    i3
     i3status
+    i3lock
     jq
     libinput
     miniserve
@@ -36,15 +44,19 @@ self: super:
     mpv
     nix-prefetch-github
     nodejs
+    pax-rs
     p7zip
     pavucontrol
     pstree
     pwgen
+    python3
     ripgrep
+    rsync
     sbt
     sqlite
     slurp
     st
+    # sway
     tigervnc
     tmux
     tree
@@ -52,10 +64,13 @@ self: super:
     usbutils
     v4l_utils
     vim
-    vlc
+    virt-viewer
+    # vlc
     vscode
+    waybar
     wget
     wlroots
+    xpra
     youtube-dl
     zip
     ;
@@ -66,10 +81,14 @@ self: super:
     cabal2nix
     ghcid
     hasktags
-    hoogle
+    # hoogle
     hpack
     nix-derivation
-    stack
+    ghc
+    fast-tags
+    nix-diff
+    # ghcide
+    # stack
     ;
   });
 }
