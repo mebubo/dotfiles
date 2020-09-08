59b95138f2aab8e23595620110505a702d227982
Sergei Dolgov <dolgovs@gmail.com>
2020-09-08 19:04:00 +0200

    home-manager: new snapshot

--- .config/nixpkgs/home-manager-snapshot.nix
+++ .config/nixpkgs/home-manager-snapshot.nix
@@ -1,4 +1,4 @@
 builtins.fetchTarball {
-  url = "https://github.com/rycee/home-manager/archive/7f7348b47049e8d25fb5b98db1d6215f8f643f0d.tar.gz";
-  sha256 = "1s8j8h42k354xzwlqizbzxlqvsagx7yrxl20qfxgxmx522ajw63p";
+  url = "https://github.com/rycee/home-manager/archive/249650a07ee2d949fa599f3177a8c234adbd1bee.tar.gz";
+  sha256 = "0x858b7i15kx74aqwgi2n5ls7zjhcky95z9vbxfdlawmaz371dma";
 }
