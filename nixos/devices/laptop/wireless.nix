ad01449f2fec11b58877341e424c1d9cac842116
Sergei Dolgov <dolgovs@gmail.com>
2020-12-24 23:45:51 +0100

    Wifi qr code

--- nixos/devices/laptop/wireless.nix
+++ nixos/devices/laptop/wireless.nix
@@ -1,12 +1,25 @@
-{ ... }:
+{ pkgs, ... }:
+
+let
+  ssid = "ssid";
+  psk = "psk";
+
+  qr = pkgs.writeShellScriptBin "qr" ''
+    ${pkgs.qrencode}/bin/qrencode -o - -t ANSI "WIFI:S:${ssid};T:WPA;P:${psk};;"
+  '';
+
+
+in
 
 {
   networking.wireless = {
     enable = true;
     networks = {
-      network1 = {
-        psk = "psk1";
+      "${ssid}" = {
+        inherit psk;
       };
     };
   };
+
+  users.users.me.packages = [ qr ];
 }
