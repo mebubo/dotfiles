023699effd6a85c6331d2c9d55ae2e52cda3aeb1
Sergei Dolgov <dolgovs@gmail.com>
2021-02-27 17:06:56 +0100

    bootstrap.sh: use home-headless.nix

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,4 +6,4 @@ $DOTFILES/install-nix-2.3.10.sh --no-daemon
 
 . $HOME/.nix-profile/etc/profile.d/nix.sh
 
-NIX_PATH=$NIX_PATH:nixpkgs-overlays=$DOTFILES/nixos/overlays nix-shell -p home-manager-init --run "HOME_MANAGER_CONFIG=$DOTFILES/nixos/home-manager/home.nix home-manager switch"
+NIX_PATH=$NIX_PATH:nixpkgs-overlays=$DOTFILES/nixos/overlays nix-shell -p home-manager-init --run "HOME_MANAGER_CONFIG=$DOTFILES/nixos/home-manager/home-headless.nix home-manager switch"
