50164485e0b1cb170130bf52d0e53607e749b45c
Sergei Dolgov <dolgovs@gmail.com>
2021-02-27 17:04:58 +0100

    neovim: update snapshot

--- nixos/overlays/50-neovim.nix
+++ nixos/overlays/50-neovim.nix
@@ -2,10 +2,10 @@ self: super: {
 
   neovim-unwrapped = super.neovim-unwrapped.overrideAttrs (oldAttrs: rec {
 	name = "neovim-nightly";
-	version = "0.5-nightly";
+	version = "0.5-nightly-2021-02-27";
 	src = self.fetchurl {
-	  url = "https://github.com/neovim/neovim/commit/8c8cc35926f265bf4f048b83fd130bef3932851e.tar.gz";
-	  sha256 = "1rmxvygr9f51s01wvdxn1bkldwhd55kkxj3937dqwaqn8r23zjqv";
+	  url = "https://github.com/neovim/neovim/archive/c1fbc2ddf15b2f44b615f90b2511349ab974cb83.tar.gz";
+	  sha256 = "09sbvv124mnj1s21nd5q5y0z11r13h0n9yak0dgp6dpqf9lxa7lh";
 	};
 
 	buildInputs = oldAttrs.buildInputs ++ [ self.tree-sitter ];
