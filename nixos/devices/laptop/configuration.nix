53a630e6fafd77be84db158927e1e769cb69aee5
Sergei Dolgov <dolgovs@gmail.com>
2021-04-19 00:31:26 +0200

    nixos: disable v4l2loopback

--- nixos/devices/laptop/configuration.nix
+++ nixos/devices/laptop/configuration.nix
@@ -35,9 +35,9 @@
 
     binfmt.emulatedSystems = [ "aarch64-linux" ];
 
-    extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
-    extraModprobeConfig = ''options v4l2loopback exclusive_caps=1 video_nr=10 card_label=MyCamera'';
-    kernelModules = [ "v4l2loopback" ];
+    # extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
+    # extraModprobeConfig = ''options v4l2loopback exclusive_caps=1 video_nr=10 card_label=MyCamera'';
+    # kernelModules = [ "v4l2loopback" ];
 
   };
 
