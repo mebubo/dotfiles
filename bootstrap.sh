266636556a06a3807d6eae1f149036f2f1bc95e1
Sergei Dolgov <dolgovs@gmail.com>
2021-06-20 14:37:37 +0200

    bootstrap.sh: set -x

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,8 @@ DOTFILES=$(dirname "$0")
 
 $DOTFILES/install-nix-2.3.10.sh --no-daemon
 
+set -x
+
 . $HOME/.nix-profile/etc/profile.d/nix.sh
 
 NIX_PATH=$NIX_PATH:nixpkgs-overlays=$DOTFILES/nixos/overlays nix-shell -p home-manager-init --run "HOME_MANAGER_CONFIG=$DOTFILES/nixos/home-manager/home-headless.nix home-manager switch"
