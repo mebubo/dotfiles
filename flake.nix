80990fa261e122b1f6f39550e4188c411bbaf86e
Sergei Dolgov <dolgovs@gmail.com>
2022-01-22 23:11:43 +0100

    flake.nix

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "NixOS configuration";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations = {
+      laptop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./nixos/devices/laptop/configuration.nix
+        ];
+      };
+    };
+  };
+}
