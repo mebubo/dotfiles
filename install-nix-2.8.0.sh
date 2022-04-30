4f3053e877501a8a71b6030868344607baa19d2b
Sergei Dolgov <dolgovs@gmail.com>
2022-04-30 14:15:57 +0200

    install-nix-2.8.0.sh

--- install-nix-2.8.0.sh
+++ install-nix-2.8.0.sh
@@ -26,38 +26,38 @@ require_util() {
 
 case "$(uname -s).$(uname -m)" in
     Linux.x86_64)
-        hash=e72114e4858b01e1b44048408ec1c21a988f4aad10559f1a821664dcc4e5b27d
-        path=xhnmj5qfvy016acwbvfzvynx444hmz45/nix-2.5.1-x86_64-linux.tar.xz
+        hash=0b32afd8c9147532bf8ce8908395b1b4d6dde9bedb0fcf5ace8b9fe0bd4c075c
+        path=0zij5bm5f2gm3p2c8dkkv58684j1k100/nix-2.8.0-x86_64-linux.tar.xz
         system=x86_64-linux
         ;;
     Linux.i?86)
-        hash=72b73f7f6b757ed45bce3c4fd22e6117aa6dcc9c36083462dd59e9a1258aeba5
-        path=s6k7m8ki8jrk8k8gakrs6zqm88z7b7a0/nix-2.5.1-i686-linux.tar.xz
+        hash=3f4bb50f639515df069fb682bb68da77565e5ca8678a3b0fb7dcc79ef591f518
+        path=kjpj1rn6x5lh20fkyfyyzgmgjdra1jpy/nix-2.8.0-i686-linux.tar.xz
         system=i686-linux
         ;;
     Linux.aarch64)
-        hash=5cd83ba4f4e8c7f9350ad392acc24940d7ddf0be12503d5719a5ae943306792d
-        path=facx9jfwad60lr6vlhh71jgy7x09kg2w/nix-2.5.1-aarch64-linux.tar.xz
+        hash=d29ea31c581e1ba7a651e6b22999cef8923e852e1d6fe7008d9545f4275f5343
+        path=npadny2da5149lcycbfmacf1r936n9zg/nix-2.8.0-aarch64-linux.tar.xz
         system=aarch64-linux
         ;;
     Linux.armv6l_linux)
-        hash=1b3bdc3e5c67855365e9dd9f91fee127d45aa36e03386852742c8a6e4f31963c
-        path=zz8pnbv5kaz3yr4jx0c20pa0x29viw71/nix-2.5.1-armv6l-linux.tar.xz
+        hash=69d5cb0e95bc83154099debd139d4f767622d94b17149fa127d492017c2e3896
+        path=jb1l7y40im5dsbq5gamppss59y0c7jmj/nix-2.8.0-armv6l-linux.tar.xz
         system=armv6l-linux
         ;;
     Linux.armv7l_linux)
-        hash=4bde07a9adf7d59f432f117b9cd5db7612e6e479594c5023bd4fdd38e63e2e5d
-        path=b35i9mlniigb11ddr5vc11r0rz044m9x/nix-2.5.1-armv7l-linux.tar.xz
+        hash=25857729f23dc25fe92dabd376917d83fe0f23038f82c1f2ab230171eb70f648
+        path=firp24ikxcygwrwd4208lyla4b6jl3sh/nix-2.8.0-armv7l-linux.tar.xz
         system=armv7l-linux
         ;;
     Darwin.x86_64)
-        hash=b84f6acba7ad0e27c46b87151d4ed4810af29595c76f5af33c6dedf28559aa4a
-        path=fh7dr2g1c6aldwb1jkdgb9nzqaabfqxs/nix-2.5.1-x86_64-darwin.tar.xz
+        hash=ebf383f1b499d3e4897cd61d068dc46e118e5f53667f5f28748b0b3682d7649a
+        path=wwf7b61nyhgj3z0vvgnnb4yzi081jkjp/nix-2.8.0-x86_64-darwin.tar.xz
         system=x86_64-darwin
         ;;
     Darwin.arm64|Darwin.aarch64)
-        hash=911ba8bdc4852451117bc5609588ad22043b68a4f7c4495d89c80289a83b1e3c
-        path=7wzj75450x4c9sg7aai8p9pq19xlf03j/nix-2.5.1-aarch64-darwin.tar.xz
+        hash=f320f381299e0fc2f907ae81ac123d0689245cb39f0672f8a65dffea12fa0240
+        path=fr5rcinvqzgcrggxw3phrzcck9wpzz83/nix-2.8.0-aarch64-darwin.tar.xz
         system=aarch64-darwin
         ;;
     *) oops "sorry, there is no binary distribution of Nix for your platform";;
@@ -71,10 +71,10 @@ if [ "${1:-}" = "--tarball-url-prefix" ]; then
     url=${2}/${path}
     shift 2
 else
-    url=https://releases.nixos.org/nix/nix-2.5.1/nix-2.5.1-$system.tar.xz
+    url=https://releases.nixos.org/nix/nix-2.8.0/nix-2.8.0-$system.tar.xz
 fi
 
-tarball=$tmpDir/nix-2.5.1-$system.tar.xz
+tarball=$tmpDir/nix-2.8.0-$system.tar.xz
 
 require_util tar "unpack the binary tarball"
 if [ "$(uname -s)" != "Darwin" ]; then
@@ -82,14 +82,14 @@ if [ "$(uname -s)" != "Darwin" ]; then
 fi
 
 if command -v curl > /dev/null 2>&1; then
-    fetch() { curl -L "$1" -o "$2"; }
+    fetch() { curl --fail -L "$1" -o "$2"; }
 elif command -v wget > /dev/null 2>&1; then
     fetch() { wget "$1" -O "$2"; }
 else
     oops "you don't have wget or curl installed, which I need to download the binary tarball"
 fi
 
-echo "downloading Nix 2.5.1 binary tarball for $system from '$url' to '$tmpDir'..."
+echo "downloading Nix 2.8.0 binary tarball for $system from '$url' to '$tmpDir'..."
 fetch "$url" "$tarball" || oops "failed to download '$url'"
 
 if command -v sha256sum > /dev/null 2>&1; then
