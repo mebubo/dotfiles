30509d21efa57a1d1c6fca42431a96adac623d6a
Sergei Dolgov <dolgovs@gmail.com>
2022-05-15 09:30:16 +0200

    home-manager-snapshot.nix: update

--- nixos/home-manager/home-manager-snapshot.nix
+++ nixos/home-manager/home-manager-snapshot.nix
@@ -1,4 +1,4 @@
 builtins.fetchTarball {
-  url = "https://github.com/nix-community/home-manager/archive/3d46c011d2cc2c9ca24d9b803e9daf156d9429ea.tar.gz";
-  sha256 = "1vbk85w03bd636lngs08q7xvikajllb9sh21ndzmzqrs3p0rglwk";
+  url = "https://github.com/nix-community/home-manager/archive/273598f53e04f0111dca5724b37640e3907edaaf.tar.gz";
+  sha256 = "0k3665p48amvdiqdq73pqfrgxjm3984k14avhdd4wakza1syc7cy";
 }
