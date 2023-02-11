67a9a4d363fedc0b1f9e0935db6fd5c9275db8f1
Sergei Dolgov <dolgovs@gmail.com>
2023-02-11 11:51:49 +0100

    vscodium: enable and confgure

--- nixos/home-manager/home-desktop.nix
+++ nixos/home-manager/home-desktop.nix
@@ -49,6 +49,46 @@
         pkgs.obs-studio-plugins.wlrobs
       ];
     };
+    vscode = {
+      enable = true;
+      enableUpdateCheck = false;
+      enableExtensionUpdateCheck = false;
+      package = pkgs.vscodium;
+      extensions = with pkgs.vscode-extensions; [
+        vscodevim.vim
+        ms-pyright.pyright
+        # scalameta.metals
+      ];
+      mutableExtensionsDir = false;
+      userSettings = {
+        "editor.cursorBlinking" = "solid";
+        "editor.fontSize" = 11;
+        "editor.lineHeight" = 15;
+        "editor.minimap.enabled" = false;
+        "explorer.confirmDelete" = false;
+        "explorer.confirmDragAndDrop" = false;
+        "extensions.autoUpdate" = false;
+        "files.autoSave" = "onFocusChange";
+        "files.trimTrailingWhitespace" = true;
+        "telemetry.enableCrashReporter" = false;
+        "telemetry.enableTelemetry" = false;
+        "vim.handleKeys" = {
+            "<C-k>" = false;
+            "<C-t>" = false;
+        };
+        "vim.hlsearch" = true;
+        "window.menuBarVisibility" = "classic";
+        "window.restoreWindows" = "none";
+        "window.titleBarStyle" = "custom";
+        # "window.experimental.useSandbox" = true;
+        "workbench.activityBar.visible" = true;
+        # "workbench.colorTheme" = "Default Light+";
+        "workbench.editor.showTabs" = false;
+        "workbench.editor.tabCloseButton" = "off";
+        "workbench.tree.indent" = 16;
+        "typescript.updateImportsOnFileMove.enabled" = "always";
+      };
+    };
   };
 
   xdg.configFile = {
