f0b85116055941fbc76be5944e7ca2dc0249ba8e
Sergei Dolgov <dolgovs@gmail.com>
2023-05-11 18:42:35 +0200

    modules/home-manager.nix: add overlays

--- nixos/modules/home-manager.nix
+++ nixos/modules/home-manager.nix
@@ -10,10 +10,16 @@ in
   imports = [ "${home-manager-shapshot}/nixos" ];
 
   home-manager = {
-    useGlobalPkgs = true;
+    useGlobalPkgs = false;
     useUserPackages = true;
     users = {
       me = import ../home-manager/home.nix;
     };
   };
+
+  nixpkgs.overlays = [
+    (import ../overlays/50-home-manager.nix)
+    (import ../overlays/50-vim-plugins.nix)
+  ];
+
 }
