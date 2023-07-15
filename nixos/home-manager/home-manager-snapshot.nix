678db4f7524ead532f9463f47eed8acdb6371503
Sergei Dolgov <dolgovs@gmail.com>
2023-07-15 18:35:39 +0200

    home-manager: cleanup

--- nixos/home-manager/home-manager-snapshot.nix
+++
@@ -1,4 +0,0 @@
-builtins.fetchTarball {
-  url = "https://github.com/nix-community/home-manager/archive/39c7d0a97a77d3f31953941767a0822c94dc01f5.tar.gz";
-  sha256 = "1hsspmy78w28lsrvvqky86nxlrb0w86v56csrr1aqqhmmlyk3z90";
-}
