5c236b4a25cde33fd56444110e7494b13d503650
Sergei Dolgov <dolgovs@gmail.com>
2023-07-15 20:08:54 +0200

    nixos/devices/laptop/wireless.nix: improve the qr script

--- nixos/devices/laptop/wireless.nix
+++ nixos/devices/laptop/wireless.nix
@@ -1,12 +1,20 @@
 { dotfiles-private }:
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 let
-  ssid = "ssid";
-  psk = "psk";
+
+  networks = (import dotfiles-private).wireless.networks;
+
+  script-line = ssid: network: ''
+    test "$1" = "${ssid}" && ${pkgs.qrencode}/bin/qrencode -o - -t ANSI "WIFI:S:${ssid};T:WPA;P:${network.psk};;"
+  '';
+
+  psk-networks = lib.filterAttrs (_: v: builtins.hasAttr "psk" v) networks;
+  script = builtins.concatStringsSep "\n" (lib.mapAttrsToList script-line psk-networks);
 
   qr = pkgs.writeShellScriptBin "qr" ''
-    ${pkgs.qrencode}/bin/qrencode -o - -t ANSI "WIFI:S:${ssid};T:WPA;P:${psk};;"
+    set -ue
+    ${script}
   '';
 
 
@@ -16,7 +24,7 @@ in
   networking.wireless = {
     enable = true;
     interfaces = [ "wlp3s0" ];
-    networks = (import dotfiles-private).wireless.networks;
+    inherit networks;
     userControlled = {
       enable = true;
       group = "wireless";
