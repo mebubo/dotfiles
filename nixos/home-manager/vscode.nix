3630e913e2ea4a365e96a8d16b5358ff9b59f981
Sergei Dolgov <dolgovs@gmail.com>
2023-11-14 20:38:28 +0100

    vscode: 1.84

--- nixos/home-manager/vscode.nix
+++ nixos/home-manager/vscode.nix
@@ -31,9 +31,9 @@ pkgs: package: {
     "window.restoreWindows" = "none";
     "window.titleBarStyle" = "custom";
     "window.density.editorTabHeight" = "compact";
-    "workbench.activityBar.visible" = true;
+    "workbench.activityBar.location" = "top";
     "workbench.colorTheme" = "Default Light Modern";
-    "workbench.editor.showTabs" = true;
+    "workbench.editor.showTabs" = "multiple";
     "workbench.editor.tabCloseButton" = "right";
     "workbench.editor.pinnedTabsOnSeparateRow" = true;
     "workbench.tree.indent" = 24;
