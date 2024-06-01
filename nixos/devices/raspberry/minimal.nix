69bd026b999812da8006149497d1e812f8094d2a
Sergei Dolgov <dolgovs@gmail.com>
2024-06-01 15:22:23 +0200

    raspberry: don't use the latest kernel, doesn't boot

--- nixos/devices/raspberry/minimal.nix
+++ nixos/devices/raspberry/minimal.nix
@@ -6,7 +6,7 @@
   boot.loader.generic-extlinux-compatible.enable = true;
   # boot.consoleLogLevel = lib.mkDefault 7;
   boot.kernelParams = ["console=ttyS0,115200n8" "console=ttyAMA0,115200n8" "console=tty0"];
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.initrd.availableKernelModules = [ "vc4" "bcm2835_dma" "i2c_bcm2835" ];
   boot.supportedFilesystems = lib.mkForce [ "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" "ext4" "vfat" ];
 
