ef68aa4f32b9ed2c777bf902d726214eed90fc6c
Sergei Dolgov <dolgovs@gmail.com>
2024-09-11 01:18:24 +0200

    Cleanup

--- nixos/devices/fr/configuration.nix
+++ nixos/devices/fr/configuration.nix
@@ -29,36 +29,6 @@ in
 
   time.timeZone = "Europe/Paris";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkbOptions in tty.
-  # };
-
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = {
-  #   "eurosign:e";
-  #   "caps:escape" # map caps to escape.
-  # };
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.me = {
     isNormalUser = true;
     extraGroups = [ "systemd-journal" "libvirtd" "wireless" ];
@@ -94,20 +64,8 @@ in
 
   networking.firewall.enable = true;
   networking.firewall.allowedTCPPorts = [ 8000 8090 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.11"; # Did you read the comment?
+  system.stateVersion = "22.11";
 
   nixpkgs.config = {
     allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
@@ -119,16 +77,11 @@ in
     ];
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
   programs = {
-    gnupg.agent = { enable = true; enableSSHSupport = false; };
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = false;
+    };
     ssh.startAgent = true;
     sway = {
       enable = true;
@@ -207,10 +160,7 @@ in
     systemPackages = with pkgs; [
       vim
       git
-      # curl
-      # file
-      # tmux
-      # htop
+      curl
     ];
 
     etc."resolv.conf".text = ''
