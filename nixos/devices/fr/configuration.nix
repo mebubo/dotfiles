8bc1a1edf0ffdb5b26923a8f206f4390c1e850b2
Sergei Dolgov <dolgovs@gmail.com>
2024-09-12 08:28:31 +0200

    ollama

--- nixos/devices/fr/configuration.nix
+++ nixos/devices/fr/configuration.nix
@@ -183,6 +183,25 @@ in
     font-awesome
   ];
 
+  services = {
+    ollama = {
+      enable = true;
+    };
+    open-webui = {
+      enable = true;
+      environment = {
+        ANONYMIZED_TELEMETRY = "False";
+        DO_NOT_TRACK = "True";
+        SCARF_NO_ANALYTICS = "True";
+        # OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
+        WEBUI_AUTH = "False";
+      };
+    };
+    # private-gpt = {
+    #   enable = true;
+    # };
+  };
+
   services.power-profiles-daemon.enable = true;
 
   services.logind = {
