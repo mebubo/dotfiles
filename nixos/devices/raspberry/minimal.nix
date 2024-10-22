f215ed48d32a4714cf00a04195d33e9ea38099fa
Sergei Dolgov <dolgovs@gmail.com>
2024-10-22 03:28:57 +0200

    raspberry: always load modules for early boot messages as per wiki; reduce the supported filesystems set

--- nixos/devices/raspberry/minimal.nix
+++ nixos/devices/raspberry/minimal.nix
@@ -7,8 +7,8 @@
   # boot.consoleLogLevel = lib.mkDefault 7;
   boot.kernelParams = ["console=ttyS0,115200n8" "console=ttyAMA0,115200n8" "console=tty0"];
   # boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.initrd.availableKernelModules = [ "vc4" "bcm2835_dma" "i2c_bcm2835" ];
-  boot.supportedFilesystems = lib.mkForce [ "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" "ext4" "vfat" ];
+  boot.initrd.kernelModules = [ "vc4" "bcm2835_dma" "i2c_bcm2835" ];
+  boot.supportedFilesystems = lib.mkForce [ "vfat" "f2fs" "ext4" ];
 
   hardware.enableRedistributableFirmware = true;
 
