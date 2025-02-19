e0c5369a58820a98c073b34925bff5264a0b152d
Sergei Dolgov <dolgovs@gmail.com>
2025-02-19 18:01:30 +0100

    open-webui: re-enable

--- nixos/devices/fr/configuration.nix
+++ nixos/devices/fr/configuration.nix
@@ -208,12 +208,12 @@ in
       enable = true;
     };
     open-webui = {
-      enable = false;
+      enable = true;
       environment = {
         ANONYMIZED_TELEMETRY = "False";
         DO_NOT_TRACK = "True";
         SCARF_NO_ANALYTICS = "True";
-        # OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
+        OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
         WEBUI_AUTH = "False";
       };
     };
