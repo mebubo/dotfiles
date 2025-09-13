ea6033e7f92c82c7c030b8c123d205c859126c20
Sergei Dolgov <dolgovs@gmail.com>
2025-09-13 16:11:06 +0200

    swap file, commented out

--- nixos/devices/fr/hardware-configuration.nix
+++ nixos/devices/fr/hardware-configuration.nix
@@ -33,7 +33,10 @@
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  swapDevices = [ ];
+  # swapDevices = [{
+  #   device = "/var/lib/swapfile";
+  #   size = 64*1024;
+  # }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
