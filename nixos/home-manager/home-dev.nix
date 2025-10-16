17a0c4b96667f5d535704487e1fd2ba8fc3e9a1c
Sergei Dolgov <dolgovs@gmail.com>
2025-10-16 06:45:12 +0200

    helix

--- nixos/home-manager/home-dev.nix
+++ nixos/home-manager/home-dev.nix
@@ -79,6 +79,7 @@ in
     # goose-cli
     # claude-code
     windsurf
+    helix
     dev-home-manager-rebuild
   ];
 
@@ -91,6 +92,58 @@ in
       sessionVariables = {
         JAVA_HOME = "${pkgs.jdk}";
         # LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib/";
+      };
+    };
+    helix = {
+      enable = true;
+      languages = {
+        language-server = {
+          typescript-language-server = {
+            command = lib.getExe pkgs.typescript-language-server;
+          };
+          nil = {
+            command = lib.getExe pkgs.nil;
+          };
+          ty = {
+            command = lib.getExe pkgs.ty;
+          };
+          metals = {
+            command = lib.getExe pkgs.metals;
+          };
+          harper-ls = {
+            command = "${pkgs.harper}/bin/harper-ls";
+          };
+          markdown-oxide = {
+            command = lib.getExe pkgs.markdown-oxide;
+          };
+        };
+        language = [
+        {
+          name = "typescript";
+          auto-format = false;
+          language-servers = [ "typescript-language-server" "harper-ls" ];
+        }
+        {
+          name = "nix";
+          auto-format = false;
+          language-servers = [ "nil" "harper-ls" ];
+        }
+        {
+          name = "python";
+          auto-format = false;
+          language-servers = [ "ty" "harper-ls" ];
+        }
+        {
+          name = "scala";
+          auto-format = false;
+          language-servers = [ "metals" "harper-ls" ];
+        }
+        {
+          name = "markdown";
+          auto-format = false;
+          language-servers = [ "markdown-oxide" "harper-ls" ];
+        }
+        ];
       };
     };
   };
