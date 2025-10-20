6595d58f8d7dcd9498fd903cf78edc664095d600
Sergei Dolgov <dolgovs@gmail.com>
2025-10-20 18:42:25 +0200

    cursor: new version

--- nixos/home-manager/home-dev.nix
+++ nixos/home-manager/home-dev.nix
@@ -27,9 +27,9 @@ my-cursor =
 
   let
 
-    version = "1.7.46";
-    url = "https://downloads.cursor.com/production/b9e5948c1ad20443a5cecba6b84a3c9b99d62582/linux/x64/Cursor-1.7.46-x86_64.AppImage";
-    hash = "sha256-XDKDZYCagr7bEL4HzQFkhdUhPiL5MaRzZTPNrLDPZDM=";
+    version = "1.7.52";
+    url = "https://downloads.cursor.com/production/9675251a06b1314d50ff34b0cbe5109b78f848cd/linux/x64/Cursor-1.7.52-x86_64.AppImage";
+    hash = "sha256-nhDDdXE5/m9uASiQUJ4GHfApkzkf9ju5b8s0h6BhpjQ=";
 
   in
 
