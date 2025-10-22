64f6b5dc52b55f31346fba6110bd4c75562da464
Sergei Dolgov <dolgovs@gmail.com>
2025-10-22 13:10:00 +0200

    Cleanup

--- bin/allow.sh
+++
@@ -1,3 +0,0 @@
-chmod g+rx $XDG_RUNTIME_DIR
-chmod g+rwx $XDG_RUNTIME_DIR/wayland-1
-chmod g+rw $XDG_RUNTIME_DIR/wayland-1.lock
--- bin/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export NIXOS_OZONE_WL=1
-export WAYLAND_DISPLAY=wayland-1
-export XDG_RUNTIME_DIR=/home/dev/xdg
-export XDG_SESSION_TYPE=wayland
-
-$@
