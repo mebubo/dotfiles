66a4334ae14ed2983ad35768c8bb2390e47bfdc4
Sergei Dolgov <dolgovs@gmail.com>
2025-11-11 17:48:02 +0100

    raspberry: home-assistant: run as user

--- nixos/devices/raspberry/home-assistant-upstream.nix
+++ nixos/devices/raspberry/home-assistant-upstream.nix
@@ -1,6 +1,20 @@
 { config, pkgs, lib, ... }:
 
 {
+  virtualisation.podman.enable = true;
+
+  users.users.podman-home-assistant = {
+    isSystemUser = true;
+    group = "podman-home-assistant";
+    description = "Home Assistant (rootless Podman)";
+    home = "/var/lib/podman-home-assistant";
+    createHome = true;
+    # required for rootless user namespaces
+    subUidRanges = [{ startUid = 100000; count = 65536; }];
+    subGidRanges = [{ startGid = 100000; count = 65536; }];
+  };
+  users.groups.podman-home-assistant = {};
+
   virtualisation.oci-containers = {
     backend = "podman";
     containers.homeassistant = {
@@ -11,6 +25,7 @@
       extraOptions = [
         "--network=host"
       ];
+      podman.user = "podman-home-assistant";
     };
   };
   networking.firewall.allowedTCPPorts = [ 8123 ];
