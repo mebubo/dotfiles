10af7427b1333750f1eb41502b69ac8299a3bb6d
Sergei Dolgov <dolgovs@gmail.com>
2026-03-08 05:59:40 +0100

    router.nix: use nixos firewall with the nftables backend

--- nixos/devices/raspberry/router.nix
+++ nixos/devices/raspberry/router.nix
@@ -8,6 +8,7 @@ let
   countryCode = router.countryCode;
   apAddress = "10.10.10.1";
   apDhcpRange = "10.10.10.100,10.10.10.200,24h";
+  apSubnet = "10.10.10.0/24";
   upstreamRouter = "192.168.1.254";
 
   qr = pkgs.writeShellScriptBin "qr" ''
@@ -48,6 +49,7 @@ in
 
   services.dnsmasq = {
     enable = true;
+    resolveLocalQueries = false;
     settings = {
       interface = "wlan0";
       bind-interfaces = true;
@@ -72,7 +74,7 @@ in
       prefixLength = 24;
     }];
     interfaces.eth0.ipv4.addresses = [{
-      address = "192.168.1.44";
+      address = "192.168.1.122";
       prefixLength = 24;
     }];
     defaultGateway = {
@@ -83,59 +85,36 @@ in
     useDHCP = false;
   };
 
+  networking.firewall = {
+    enable = true;
+    backend = "nftables";
+    filterForward = true;
+    interfaces.wlan0.allowedUDPPorts = [ 53 67 ];
+    allowedTCPPorts = [ 22 ];
+    extraForwardRules = ''
+      # Block DNS to the upstream router
+      iifname "wlan0" ip daddr ${upstreamRouter} udp dport 53 drop
+      iifname "wlan0" ip daddr ${upstreamRouter} tcp dport 53 drop
+
+      # Allow WiFi clients to reach the home network (RFC1918 ranges)
+      iifname "wlan0" ip daddr 192.168.0.0/16 accept
+      iifname "wlan0" ip daddr 10.0.0.0/8 accept
+      iifname "wlan0" ip daddr 172.16.0.0/12 accept
+    '';
+  };
+
   networking.nftables = {
     enable = true;
-    ruleset = ''
-      table inet filter {
-        chain input {
-          type filter hook input priority 0; policy drop;
-
-          iif "lo" accept
-          ct state established,related accept
-
-          # Allow DHCP and DNS from WiFi clients
-          iifname "wlan0" udp dport { 53, 67 } accept
-
-          # Allow SSH (adjust as needed)
-          tcp dport 22 accept
-
-          # Allow ping
-          icmp type echo-request accept
-        }
-
-        chain forward {
-          type filter hook forward priority 0; policy drop;
-
-          ct state established,related accept
-
-          # Block DNS to the upstream router
-          iifname "wlan0" ip daddr ${upstreamRouter} udp dport 53 drop
-          iifname "wlan0" ip daddr ${upstreamRouter} tcp dport 53 drop
-
-          # Allow WiFi clients to reach the home network (RFC1918 ranges)
-          iifname "wlan0" ip daddr 192.168.0.0/16 accept
-          iifname "wlan0" ip daddr 10.0.0.0/8 accept
-          iifname "wlan0" ip daddr 172.16.0.0/12 accept
-
-          # Everything else from wlan0 is dropped (no internet)
-        }
-
-        chain output {
-          type filter hook output priority 0; policy accept;
-        }
-      }
-
-      table inet nat {
+    tables."router-nat" = {
+      family = "ip";
+      content = ''
         chain postrouting {
           type nat hook postrouting priority 100;
 
           # NAT for traffic going to home network
-          oifname "eth0" masquerade
+          ip saddr ${apSubnet} oifname "eth0" masquerade
         }
-      }
-    '';
+      '';
+    };
   };
-
-  # Disable default firewall since we're using nftables directly
-  networking.firewall.enable = false;
 }
